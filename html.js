230f3866ca96ca5c6157c6db7f86841e805e9ef7
Alina Maslo <32433251+alina-maslo@users.noreply.github.com>
2018-03-12 07:47:57 +0000

    fix meta string

--- html.js
+++ html.js
@@ -22,14 +22,11 @@ module.exports = React.createClass({
     }
 
     return (
-      <html lang="en">
+      <html lang="ru">
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           {head.title.toComponent()}
           {head.meta.toComponent()}
           <TypographyStyle typography={typography} />
